d39ce50026192fd2e399a127c5373689f30fa441
sehun <sehun@sehuns-mbp.attlocal.net>
2016-12-20 20:20:26 -0800

    updated blog with slideshow text

---
+++ js/about.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){ 
+    $(function(){
+        $('#text-slide .text-item:gt(0)').hide();
+        setInterval(function(){
+          $('#text-slide :first-child').fadeOut(700)
+             .next('.text-item').fadeIn(1100)
+             .end().appendTo('#text-slide');}, 
+          4500);
+    });
+});
+
+// $(document).ready(function() {
+//     setupRotator();
+// });
+
+// function setupRotator() {
+//     if($('.text-item').length > 1) {
+//         $('.text-item:first').addClass('text-underlined').fadeIn(500);
+//         var interval = setInterval('textRotate()', 5000);
+//     }
+// }
+
+// function textRotate() {
+//     var current = $('#text-slide > .text-underlined');
+//     if(current.next().length == 0) {
+//         current.removeClass('text-underlined').fadeOut(500);
+//         current.dequeue();
+//         $('.text-item:first').addClass('text-underlined').fadeIn(500);
+//     } else {
+//         current.removeClass('text-underlined').fadeOut(500);
+//         current.dequeue();
+//         current.next().addClass('text-underlined').fadeIn(500);
+//     }
+// }
+
