8e9467cff0318b6c6b3db02a67f8fd6d10a0d9f8
sehuno <sehuno@sehunos-mbp.attlocal.net>
2023-08-16 13:22:47 -0700

    Menu finished

--- src/App.js
+++ src/App.js
@@ -3,41 +3,36 @@ import React, {Component, useState } from 'react';
 import { motion, AnimatePresence } from "framer-motion"
 import Modal from 'react-modal';
 
-const rootTopVariants = {
-  open: { opacity: 0, y: "-100%"} ,
-  closed: { opacity: 1 },
-}
-
-const rootBottomVariants = {
-  open: { opacity: 0, y: "+100%"} ,
-  closed: { opacity: 1 },
-}
-
 function App() {
   const [isOpen, setIsOpen] = useState(false)
 
   return (
     <div className="App" style={{position:"absolute",width:"100vw", height:"100vh"}}>
-      <div
-        style={{borderRadius: "50%",width:"100px", height:"100px", left:"47%",top:"47%", backgroundColor:"white",zIndex:"1", position:"absolute"}}
-        onClick={() => setIsOpen(true)}>
-      </div>
-      <div style={{position:"absolute",display:"flex", flexDirection:"column", width:"100%", height:"100%"}}>
-        <AnimatePresence>
+      <AnimatePresence>
         { !isOpen && <motion.div
-          style={{display:"flex",backgroundColor:"lightblue", width:"100%", height:"50%"}}
-          exit={{ y: "-100%", opacity: 0 }}
-          transition={{ duration: 2 }}
-        >
-        </motion.div> }
+          style={{borderRadius: "50%",width:"100px", height:"100px", left:"47%",top:"47%", backgroundColor:"white",zIndex:"1", position:"absolute"}}
+          onClick={() => setIsOpen(!isOpen)}
+          exit={{ opacity: 0 }}
+          >
+          </motion.div>
+        }
+      </AnimatePresence>
+      <div onClick={() => setIsOpen(false)} style={{position:"absolute",display:"flex", flexDirection:"column", width:"100%", height:"100%"}}>
+        <AnimatePresence>
+          { !isOpen && <motion.div
+            style={{display:"flex",backgroundColor:"lightblue", width:"100%", height:"50%"}}
+            exit={{ y: "-100%", opacity: 0 }}
+            transition={{ duration: 1 }}
+          >
+          </motion.div> }
         </AnimatePresence>
         <AnimatePresence>
-        { !isOpen && <motion.div
-          style={{display:"flex",backgroundColor:"lightgreen", width:"100%", height:"50%"}}
-          exit={{ y: "+100%", opacity: 0 }}
-          transition={{ duration: 2 }}
-        >
-        </motion.div> }
+          { !isOpen && <motion.div
+            style={{display:"flex",backgroundColor:"lightgreen", width:"100%", height:"50%"}}
+            exit={{ y: "+100%", opacity: 0 }}
+            transition={{ duration: 1 }}
+          >
+          </motion.div> }
         </AnimatePresence>
       </div>
       {isOpen && <Menu></Menu>}
@@ -45,15 +40,25 @@ function App() {
   );
 }
 
+const menuList = [
+  { id: 1, title:"About Me"},
+  { id: 2, title:"Blog"},
+  { id: 3, title:"Projects"},
+  { id: 4, title:"Photography"},
+]
+
 function Menu() {
   return(
-    <div style={{position:"absolute"}}>
-      <ul>
-        <li>About Me</li>
-        <li>Blog</li>
-        <li>Projects</li>
-        <li>Photography</li>
-      </ul>
+    <div style={{position:"absolute", width:"30rem",height:"25rem", margin:"auto", left:"40%", top:"15%"}}>
+      {menuList.map((item, i) => (
+        <motion.div key={item.id}
+          initial={{opacity:0, translateX:-500}}
+          animate={{opacity:1, translateX:0}}
+          transition={{duration: 0.8, delay: i * 0.1}}
+        >
+          <p style={{fontSize:"4.5rem"}}>{item.title}</p>
+        </motion.div>
+      ))}
     </div>
   )
 }
