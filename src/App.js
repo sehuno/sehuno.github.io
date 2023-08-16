3cc7b5688d6314107d784f635f1c803217ad319f
sehuno <sehuno@sehunos-mbp.attlocal.net>
2023-08-16 10:56:14 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <div className="App-logo" alt="logo">ᴔ</div>
-      </header>
+    <div className="App" style={{position:"absolute",width:"100vw", height:"100vh"}}>
+      <div style={{width:"100px", height:"100px", left:"47%",top:"47%", backgroundColor:"white",zIndex:"1", position:"absolute"}}></div>
+      <div style={{display:"flex", flexDirection:"column", width:"100%", height:"100%"}}>
+        <div style={{display:"flex",backgroundColor:"lightblue", width:"100%", height:"50%"}}></div>
+        <div style={{display:"flex",backgroundColor:"lightgreen", width:"100%", height:"50%"}}></div>
+      </div>
     </div>
   );
 }
 
 export default App;
+
+  // <div className="App-logo" style={{zIndex:"1", position:"absolute"}}>ᴔ</div>
